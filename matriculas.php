a325cf52f8ff8f0ef554d725e2df0308dea21bf8
Samuel Marroni <samuelmarroni@hotmail.com>
2022-05-11 16:55:28 -0300

    Finalizando o treinamento.

--- matriculas.php
+++ matriculas.php
@@ -16,4 +16,11 @@ $novosAlunos = [
 ];
 
 $alunos2022 = [...$alunos2021, 'Samuel', ...$novosAlunos];
+array_push($alunos2022, 'Sergio', 'Helena');
+$alunos2022[] = 'Laura';
+
+array_unshift($alunos2022, 'Lucas', 'Marcelo');
+echo array_shift($alunos2022) . PHP_EOL;
+echo array_pop($alunos2022) . PHP_EOL;
+
 var_dump($alunos2022);
